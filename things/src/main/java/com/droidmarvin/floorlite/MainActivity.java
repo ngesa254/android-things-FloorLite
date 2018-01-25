e83367d3bcca15d4991a7a035c3022086351d2e0
ngesa marvin <marvinngesa@gmail.com>
2018-01-25 23:34:37 +0300

    create FoorLite LED barebones

--- things/src/main/java/com/droidmarvin/floorlite/MainActivity.java
+++ things/src/main/java/com/droidmarvin/floorlite/MainActivity.java
@@ -3,10 +3,30 @@ package com.droidmarvin.floorlite;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.android.things.pio.Gpio;
+
 public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
+
+    private static final String LED_PIN_ONE = "BCM21";
+    private static final String LED_PIN_TWO = "BCM20";
+    private static final String LED_PIN_THREE = "BCM16";
+
+
+    private Gpio mLedGioOne, mLedGioTwo, mLedGioThree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
+    private void setupLeds (){
+
+    }
+
+    private void destroyLeds(){
+
+    }
+
 }
